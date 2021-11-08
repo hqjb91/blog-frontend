9049b82c43a023597c82458bac050d8a63484213
hqjb <hqjb@hotmail.sg>
2021-11-08 10:23:57 +0800

    Test create article component

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -8,11 +8,13 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 const MATERIAL = [
   MatButtonModule, MatToolbarModule, MatIconModule, 
   MatCardModule, MatPaginatorModule, MatDividerModule,
-  MatInputModule, MatDatepickerModule, MatFormFieldModule
+  MatInputModule, MatDatepickerModule, MatFormFieldModule,
+  MatNativeDateModule
 ]
 
 @NgModule({
